a18e1de44d5c8a6813b1b7762e8b7f35612c5e16
asungkim <kimasung143@gmail.com>
2025-02-12 11:42:29 +0900

    home controller design

--- backend/src/main/java/com/example/spring_doc/domain/home/controller/HomeController.java
+++ backend/src/main/java/com/example/spring_doc/domain/home/controller/HomeController.java
@@ -1,11 +1,15 @@
 package com.example.spring_doc.domain.home.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "HomeController", description = "API 서버 홈")
 @RestController
 public class HomeController {
 
+    @Operation(summary = "API 서버 시작페이지", description = "API 서버 시작페이지입니다. api 호출은 인증 해주세요.")
     @GetMapping("/")
     public String home() {
         return "API 서버에 오신 것을 환영합니다.";
